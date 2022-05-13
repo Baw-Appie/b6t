03708e6d54211eada66dbbceeaaf04eb2a1908ff
Baw Appie <pp121324@gmail.com>
2022-05-13 07:41:36 +0000

    Update

--- start_blueboat.sh
+++ start_blueboat.sh
@@ -12,4 +12,5 @@ export AWS_SECRET_ACCESS_KEY=$MINIO_ROOT_PASSWORD
 exec /data/blueboat_server -l "0.0.0.0:3000" \
   --s3-bucket "apps" --s3-region "us-east-1" \
   --s3-endpoint "http://localhost:1932" \
-  --mds ws://localhost:2999/b --mds-local-region "local"
+  --mds ws://localhost:2999/b --mds-local-region "local" \
+  $BLUEBOAT_OPTIONS
